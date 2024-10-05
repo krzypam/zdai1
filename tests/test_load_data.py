7425f775cb6efd9dd72a900310b42c5e834dd627
krzypam <krzysztof.henryk.pampuch@gmail.com>
2024-10-05 11:50:47 +0000

    increase numer of records

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -5,4 +5,7 @@ from load_data import load_data
 
 def test_load_data():
   X_train, X_test, y_train, y_test = load_data()
-  assert len(X_train) > 100, "train data is too small"
+  assert len(X_train) > 100000, "train data is too small"
+
+if __name__ == "__main__":
+  test_load_data()
